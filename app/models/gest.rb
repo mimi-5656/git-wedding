e91246572319cfdb3172794567ca53333aa2f8f5
EC2 Default User <ec2-user@ip-172-31-0-52.ap-northeast-1.compute.internal>
2022-11-21 15:40:38 +0000

    association gest,host

--- app/models/gest.rb
+++ app/models/gest.rb
@@ -3,4 +3,6 @@ class Gest < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :host
 end
--- app/models/host.rb
+++ app/models/host.rb
@@ -3,4 +3,6 @@ class Host < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :gests, dependent: :destroy
 end
